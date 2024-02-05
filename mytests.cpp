308ce294ec1e5c7d90e60dc21de7e20e18410171
PedroGTN <pedrogtneves@gmail.com>
2024-02-05 14:31:24 -0300

    Added more tests

--- mytests.cpp
+++ mytests.cpp
@@ -4,7 +4,7 @@
 
 //Teste adição
 
-    //testes feitos pra passar
+    //testes igual
     TEST(test_add, 1_1_equals_2)
     {
         GTEST_ASSERT_EQ(add2int(1, 1), 2);
@@ -14,19 +14,25 @@
         GTEST_ASSERT_EQ(add2int(97, 23), 120);
     }
 
-    //testes feitos pra falhar
-    TEST(test_add, 1_1_equals_3)
+    //testes não igual
+    TEST(test_add, 1_1_notequals_3)
     {
-        GTEST_ASSERT_EQ(add2int(1, 1), 3) << "falhou como esperado";
+        GTEST_ASSERT_NE(add2int(1, 1), 3);
     }
-    TEST(test_add, 9_9_equals_19)
+    TEST(test_add, 9_9_notequals_19)
     {
-        GTEST_ASSERT_EQ(add2int(9, 9), 19) << "falhou como esperado";
+        GTEST_ASSERT_NE(add2int(9, 9), 19);
+    }
+
+    //teste maior que
+    TEST(test_add, 2_2_greaterthan_0)
+    {
+        GTEST_ASSERT_GT(add2int(2, 2), 0);
     }
 
 //Teste subtração
 
-    //testes feitos pra passar
+    //testes igual
     TEST(test_sub, 1_1_equals_0)
     {
         GTEST_ASSERT_EQ(sub2int(1, 1), 0);
@@ -36,19 +42,25 @@
         GTEST_ASSERT_EQ(sub2int(97, 23), 74);
     }
 
-    //testes feitos pra falhar
-    TEST(test_sub, 1_1_equals_m1)
+    //testes não igual
+    TEST(test_sub, 1_1_notequals_m1)
     {
-        GTEST_ASSERT_EQ(sub2int(1, 1), -1) << "falhou como esperado";
+        GTEST_ASSERT_NE(sub2int(1, 1), -1);
     }
-    TEST(test_sub, 9_7_equals_m2)
+    TEST(test_sub, 9_7_notequals_m2)
     {
-        GTEST_ASSERT_EQ(sub2int(9, 7), -2) << "falhou como esperado";
+        GTEST_ASSERT_NE(sub2int(9, 7), -2);
+    }
+
+    //teste menor que
+    TEST(test_sub, 9_12_lessthan_0)
+    {
+        GTEST_ASSERT_LT(sub2int(9, 12), 0);
     }
 
 //Teste multiplicação
 
-    //testes feitos pra passar
+    //testes igual
     TEST(test_mul, 1_1_equals_1)
     {
         GTEST_ASSERT_EQ(mul2int(1, 1), 1);
@@ -58,19 +70,19 @@
         GTEST_ASSERT_EQ(mul2int(55, 5), 275);
     }
 
-    //testes feitos pra falhar
-    TEST(test_mul, 1_1_equals_2)
+    //testes não igual
+    TEST(test_mul, 1_1_notequals_2)
     {
-        GTEST_ASSERT_EQ(mul2int(1, 1), 2) << "falhou como esperado";
+        GTEST_ASSERT_NE(mul2int(1, 1), 2);
     }
-    TEST(test_mul, 5_m2_equals_10)
+    TEST(test_mul, 5_m2_notequals_10)
     {
-        GTEST_ASSERT_EQ(mul2int(5, -2), 10) << "falhou como esperado";
+        GTEST_ASSERT_NE(mul2int(5, -2), 10);
     }
 
 //Teste divisão
 
-    //testes feitos pra passar
+    //testes igual
     TEST(test_div, 1_0_equals_intmin)
     {
         GTEST_ASSERT_EQ(div2int(1, 0), INT32_MIN);
@@ -80,14 +92,14 @@
         GTEST_ASSERT_EQ(div2int(55, 5), 11);
     }
 
-    //testes feitos pra falhar
-    TEST(test_div, 1_1_equals_intmax)
+    //testes não igual
+    TEST(test_div, 1_1_notequals_intmax)
     {
-        GTEST_ASSERT_EQ(div2int(1, 0), INT32_MAX) << "falhou como esperado";
+        GTEST_ASSERT_NE(div2int(1, 0), INT32_MAX);
     }
-    TEST(test_div, 5_m2_equals_2)
+    TEST(test_div, 5_m2_notequals_2)
     {
-        GTEST_ASSERT_EQ(div2int(5, -2), 2) << "falhou como esperado";
+        GTEST_ASSERT_NE(div2int(5, -2), 2);
     }
 
 
